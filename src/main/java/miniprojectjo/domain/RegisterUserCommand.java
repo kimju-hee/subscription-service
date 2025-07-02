0b7abd97aec02a167879724782256aadb6a87ef9
YDH <ydo8908@gmail.com>
2025-07-02 22:31:12 +0900

    Create RegisterUserCommand.java
    회원가입을 위한 dto 구현

---
+++ src/main/java/miniprojectjo/domain/RegisterUserCommand.java
@@ -0,0 +1,10 @@
+// RegisterUserCommand.java
+package miniprojectjo.domain;
+
+import lombok.Data;
+
+@Data
+public class RegisterUserCommand {
+    private String email;
+    private String userName;
+}
