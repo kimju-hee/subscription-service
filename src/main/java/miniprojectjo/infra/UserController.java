de949c850939e734e1b2cb9c61205e27743512d1
YDH <ydo8908@gmail.com>
2025-07-02 22:38:23 +0900

    Intermediate commint
    구독권 구입 및 회원가입 api 리퀘스트 처리 부분

--- src/main/java/miniprojectjo/infra/UserController.java
+++ src/main/java/miniprojectjo/infra/UserController.java
@@ -10,6 +10,10 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.server.ResponseStatusException;
+
+
 //<<< Clean Arch / Inbound Adaptor
 
 @RestController
@@ -20,26 +24,36 @@ public class UserController {
     @Autowired
     UserRepository userRepository;
 
+    @PostMapping("/users/register")
+        public User registerUser(@RequestBody RegisterUserCommand command) {
+        return User.register(command); 
+    }
+
     @RequestMapping(
-        value = "/users/{id}/buysubscription",
+        value = "/users/buysubscription/{Id}",
         method = RequestMethod.PUT,
         produces = "application/json;charset=UTF-8"
     )
     public User buySubscription(
-        @PathVariable(value = "id") Long id,
-        @RequestBody BuySubscriptionCommand buySubscriptionCommand,
+        @PathVariable("Id") Long Id,
         HttpServletRequest request,
         HttpServletResponse response
-    ) throws Exception {
-        System.out.println("##### /user/buySubscription  called #####");
-        Optional<User> optionalUser = userRepository.findById(id);
+        ) {
+        // 유저 조회
+        User user = userRepository.findById(Id)
+            .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "해당 유저를 찾을 수 없습니다."));
 
-        optionalUser.orElseThrow(() -> new Exception("No Entity Found"));
-        User user = optionalUser.get();
-        user.buySubscription(buySubscriptionCommand);
+        // 이미 구독 중이면 예외 처리
+        if (Boolean.TRUE.equals(user.getIsPurchase())) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "이미 구독권 사용 중입니다.");
+        }
 
-        userRepository.save(user);
-        return user;
+        // 구독 로직 수행 (도메인 로직에서 이벤트 발행 포함)
+        user.buySubscription();
+
+        // 저장 및 반환
+        return userRepository.save(user);
     }
 }
 //>>> Clean Arch / Inbound Adaptor
+
