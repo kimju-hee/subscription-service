073dc987da4fe07842307a2ad0158814a052a0ca
YWS00 <wsyoon2000@gmail.com>
2025-07-03 00:04:25 +0900

    로그인시 필요 메서드 추가

--- src/main/java/miniprojectjo/domain/UserRepository.java
+++ src/main/java/miniprojectjo/domain/UserRepository.java
@@ -1,14 +1,22 @@
 package miniprojectjo.domain;
-
+ 
 import java.util.Date;
 import java.util.List;
+//Optional 추가
+import java.util.Optional;
+ 
 import miniprojectjo.domain.*;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
-
-//<<< PoEAA / Repository
+ 
+// <<< PoEAA / Repository
 @RepositoryRestResource(collectionResourceRel = "users", path = "users")
 public interface UserRepository
-    extends PagingAndSortingRepository<User, Long> {}
+    extends PagingAndSortingRepository<User, Long> {
+ 
+    //이메일로 사용자 찾기 추가
+    Optional<User> findByEmail(String email);
+ 
+}
