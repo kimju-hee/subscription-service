0e8763039c797e01439b747ab05068af94b2af2e
YDH <ydo8908@gmail.com>
2025-07-03 10:35:55 +0900

    Update UserController.java
    api 리퀘스트 저장 방식과 적용 방식 변경

--- src/main/java/miniprojectjo/infra/UserController.java
+++ src/main/java/miniprojectjo/infra/UserController.java
@@ -1,23 +1,13 @@
 package miniprojectjo.infra;
 
-import java.util.Optional;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
 import javax.transaction.Transactional;
 import miniprojectjo.domain.*;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
 
-
-//<<< Clean Arch / Inbound Adaptor
-
 @RestController
-// @RequestMapping(value="/users")
 @Transactional
 public class UserController {
 
@@ -25,35 +15,46 @@ public class UserController {
     UserRepository userRepository;
 
     @PostMapping("/users/register")
-        public User registerUser(@RequestBody RegisterUserCommand command) {
-        return User.register(command); 
+    public User registerUser(@RequestBody RegisterUserCommand command) {
+        User user = new User();
+        user.setEmail(command.getEmail());
+        user.setUserName(command.getUserName());
+        user.setPassword(command.getPassword()); // 비밀번호 설정
+        user.setPurchase(false); // 기본값
+        user.setMessage("회원가입 완료");
+
+        userRepository.save(user);
+
+        // 이벤트 발행
+        UserRegistered userRegistered = new UserRegistered(user);
+        userRegistered.publishAfterCommit();
+
+        return user;
     }
 
-    @RequestMapping(
-        value = "/users/buysubscription/{Id}",
-        method = RequestMethod.PUT,
-        produces = "application/json;charset=UTF-8"
-    )
-    public User buySubscription(
-        @PathVariable("Id") Long Id,
-        HttpServletRequest request,
-        HttpServletResponse response
-        ) {
+    @Autowired
+    SubscriptionRepository subscriptionRepository;
+
+    @PutMapping("/users/buy-subscription")
+    public User buySubscription(@RequestBody BuySubscriptionCommand command) {
         // 유저 조회
-        User user = userRepository.findById(Id)
+        User user = userRepository.findById(command.getUserId())
             .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "해당 유저를 찾을 수 없습니다."));
 
         // 이미 구독 중이면 예외 처리
-        if (Boolean.TRUE.equals(user.getIsPurchase())) {
+        if (user.isPurchase()) {
             throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "이미 구독권 사용 중입니다.");
         }
 
-        // 구독 로직 수행 (도메인 로직에서 이벤트 발행 포함)
-        user.buySubscription();
+        // User의 isPurchase 상태 업데이트
+        user.setPurchase(true);
+        userRepository.save(user);
 
-        // 저장 및 반환
-        return userRepository.save(user);
+        // SubscriptionBought 이벤트 발행
+        SubscriptionBought subscriptionBought = new SubscriptionBought(command.getUserId(), user.isPurchase());
+        subscriptionBought.publishAfterCommit();
+
+        return user;
     }
 }
-//>>> Clean Arch / Inbound Adaptor
 
