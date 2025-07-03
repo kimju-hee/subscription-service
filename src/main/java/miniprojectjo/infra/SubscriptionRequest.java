2b2c518856faf2d520976d9c4b33dcc03d92572f
YDH <ydo8908@gmail.com>
2025-07-03 10:34:42 +0900

    Update SubscriptionRequest.java
    구독료 필드 추가

--- src/main/java/miniprojectjo/infra/SubscriptionRequest.java
+++ src/main/java/miniprojectjo/infra/SubscriptionRequest.java
@@ -6,5 +6,6 @@ import lombok.Data;
 public class SubscriptionRequest {
     private Long userId;
     private Long bookId;
+    private int subscriptionFee; // 구독료 필드 추가
     // private String webUrl; // 선택적으로 필요
 }
