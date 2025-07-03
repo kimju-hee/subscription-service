9216da9f036a6ae3541507e9adfa230276f95e58
YDH <ydo8908@gmail.com>
2025-07-03 10:28:49 +0900

    Update User.java
    내부함수 및 기능 구현을 usecontroller로 변경에 따른 수정 / 추후 포인트   레파지토리와의 테스트 과정에서 수정 필수

--- src/main/java/miniprojectjo/domain/User.java
+++ src/main/java/miniprojectjo/domain/User.java
@@ -25,48 +25,35 @@ public class User {
 
     private String userName;
 
-    private Boolean isPurchase = false;
+    private String password; // 비밀번호 필드 추가
 
-    private String meseeage;
+    private boolean isPurchase = false;
 
-    public Long getId() {
-        return this.id;
+    private int point = 0; // 포인트 필드 추가
+
+    private String message;
+
+    public void suggestFeeConversion(){
+        // TODO: 비즈니스 로직 구현
+        // 예를 들어, 사용자에게 이메일이나 알림을 보내는 로직을 추가할 수 있습니다。
+        this.setMessage("포인트 부족으로 구독에 실패했습니다. 유료 구독으로 전환하시겠습니까?");
     }
 
-    // 회원가입 구현
 
-    public static User register(RegisterUserCommand command) {
-        User user = new User();
-        user.setEmail(command.getEmail());
-        user.setUserName(command.getUserName());
-        user.setIsPurchase(false); // 기본값
-        user.setMeseeage("회원가입 완료");
-
-        repository().save(user);
-
-        // 이벤트 발행
-        UserRegistered userRegistered = new UserRegistered(user);
-        userRegistered.publishAfterCommit();
-
-        return user;
+    // 포인트 레파지토리의 포인트차감 파트와 겹치는 부분이 있어 현재 미사용!
+    
+    // 도서에 따른 구독료를 받아와서 포인트 차감하는 함수
+    // 해당 함수 호출 시, 시스템으로부터 전달받은 구독료를 인자로 전달함
+    public void deductPoint(int amount) throws InsufficientPointsException {
+        if (this.point < amount) {
+            OutOfPoint outOfPoint = new OutOfPoint();
+            outOfPoint.setUserId(this.id);
+            outOfPoint.setPoint(this.point); // 현재 유저의 포인트
+            outOfPoint.publishAfterCommit();
+            throw new InsufficientPointsException("포인트가 부족합니다.");
+        }   
+        // 포인트가 부족하면 outofpoint 이벤트 발행 및 InsufficientPointsException를 발생 -> 호출한 함수에 포인트 부족 상황을 리턴함
+        this.point -= amount;   // point 에서 구독료만큼 빼서 저장
     }
-
-    public static UserRepository repository() {
-        UserRepository userRepository = SubscriberApplication.applicationContext.getBean(
-            UserRepository.class
-        );
-        return userRepository;
-    }
-
-    //<<< Clean Arch / Port Method
-    public void buySubscription() {
-        this.isPurchase = true;
-
-        SubscriptionBought event = new SubscriptionBought(this);
-        event.publishAfterCommit();
-    }
-
-    public static void guideFeeConversionSuggestion(SubscriptionFailed subscriptionFailed){}
-    // User.java
 
 }
